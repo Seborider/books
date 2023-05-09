074427d9963260e9b926b5f29a388005264a971b
Sebastian Mayer <sebastian.mayer@check24.de>
2023-05-10 00:03:50 +0200

    add signup-successful component

--- apps/books/src/app/components/auth/signup-successful/signup-successful.component.ts
+++ apps/books/src/app/components/auth/signup-successful/signup-successful.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'books-signup-successful',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink, RouterLinkActive],
     templateUrl: './signup-successful.component.html',
     styleUrls: ['./signup-successful.component.scss'],
 })
